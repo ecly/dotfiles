dfb3f00f029a4874c8aa9e3a9557e953c2d4016a
ecly <elyne@live.dk>
2017-08-04 22:48:20 +0200

    added custom rofi script for desired look

---
+++ Scripts/rofi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+. "${HOME}/.cache/wal/colors.sh"
+
+bg=$color0
+border=$color2
+separator=$color2
+fg=$color7
+bgalt=$color0
+urgent=$color9
+fgalt=$color7
+hlbg=$color2
+hlfg=$color0
+
+# argument $1 expected to be window/run
+exec rofi -show $1 -color-window "$bg, $fg, $fg" \
+                    -color-normal "$bg, $fg, $bgalt, $hlbg, $hlfg" \
+                    -color-urgent "$bg, $urgent, $bgalt, $urgent, $fg" \
+                    -color-active "$bg, $fg, $bgalt, $hlbg, $hlfg"
