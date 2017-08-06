1b80b5da18b0feb89fae847d1a2f9b134efa387c
ecly <elyne@live.dk>
2017-08-06 19:23:13 +0200

    fixed ordering of wal/poly such that initial scratchpad terminals are beautified as well

--- Scripts/beautify.sh
+++ Scripts/beautify.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
-# Loads last used wal colorscheme, runs wal-set to start dunst with right colors, and runs polybar launch script
+# Waits for wal to set colors and then starts polybar and wal-set which launches dunst
 
-# Generate correct path for last used colorscheme
-scheme=$(cat ~/.cache/wal/wal)
-scheme=${scheme/./_} #replace . with _
-ext=".json"
-
-wal -f "${HOME}/.cache/wal/schemes/$scheme$ext" -o ~/Scripts/bin/wal-set && ~/.config/polybar/launch.sh
+while true; do
+    if [[ ! $color0 ]]; then
+        sh ~/.config/polybar/launch.sh && ~/Scripts/bin/wal-set
+        break
+    fi
+    sleep 0.1
+done
