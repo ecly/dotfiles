cdfd3c9ea78a7a4346a42f868fa59ea36e5378ee
ecly <elyne@live.dk>
2017-08-07 23:48:21 +0200

    redistributed sleep to fix poly with x bug to i3c

--- Scripts/beautify.sh
+++ Scripts/beautify.sh
@@ -1,11 +1,5 @@
 #!/bin/bash 
 
-# Currently wait a bit before executing due to a known bug
-# https://bbs.archlinux.org/viewtopic.php?id=200797
-# Modify sleep time from machine to machine
-
-sleep 1
-
 # Waits for wal to set colors and then starts polybar and wal-set which launches dunst
 while true; do
     if [[ ! $color15 ]]; then
