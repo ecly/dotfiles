81f219b24ca9f4ad813fa310e2f005d403465db1
ecly <elyne@live.dk>
2017-08-07 23:26:42 +0200

    completely simplified theming from ranger

--- Scripts/beautify.sh
+++ Scripts/beautify.sh
@@ -9,8 +9,8 @@ sleep 1
 # Waits for wal to set colors and then starts polybar and wal-set which launches dunst
 while true; do
     if [[ ! $color15 ]]; then
-        sh ~/.config/polybar/launch.sh &  
-        ~/Scripts/bin/wal-set &
+        ~/Scripts/bin/wal-set
+        sh ~/.config/polybar/launch.sh
         exit 1
     fi
     sleep 0.1
