10bc39f6ea4ad54e89ee8bbf7da01e96dad63b66
ecly <elyne@live.dk>
2017-08-07 23:10:17 +0200

    solved issue with poly not corretly launching on boot

--- Scripts/beautify.sh
+++ Scripts/beautify.sh
@@ -1,10 +1,17 @@
 #!/bin/bash 
-# Waits for wal to set colors and then starts polybar and wal-set which launches dunst
 
+# Currently wait a bit before executing due to a known bug
+# https://bbs.archlinux.org/viewtopic.php?id=200797
+# Modify sleep time from machine to machine
+
+sleep 1
+
+# Waits for wal to set colors and then starts polybar and wal-set which launches dunst
 while true; do
     if [[ ! $color15 ]]; then
-        sh ~/.config/polybar/launch.sh && ~/Scripts/bin/wal-set
-        break
+        sh ~/.config/polybar/launch.sh &  
+        ~/Scripts/bin/wal-set &
+        exit 1
     fi
     sleep 0.1
 done
