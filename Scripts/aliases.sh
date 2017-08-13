f90ba7da7daf87af320c0fd996bcc10ed6a5e152
ecly <elyne@live.dk>
2017-08-13 12:33:48 +0200

    updates to mutt

--- Scripts/aliases.sh
+++ Scripts/aliases.sh
@@ -1,20 +1,36 @@
 # ALIASES
-alias ls="ls-i --color=auto"
-alias dir="dir-i --color=auto"
-alias vdir="vdir-i --color=auto"
-alias pacyyu="sudo pacman -Syyu"
-alias ls="ls --color"
 alias shutdown="systemctl poweroff"
-alias pacyy="sudo pacman -Syy"
-alias pacyu="sudo pacman -Syu"
 alias vim="vim"
 alias pdf="zathura"
 alias poly="sh ~/.config/polybar/launch.sh"
-alias rm="rm -I"
+alias rm="rm -I" # ask when deleting multiple
 alias gsync="grive -p ~/GoogleDrive"
+
 #Basically an ls replacement for tree
 alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'"
+
+#A tree function for files as well
 alias ftree="find | sed 's|[^/]*/|- |g'"
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+  colorflag="--color"
+else # OS X `ls`
+  colorflag="-G"
+fi
+
+# Basic colored ls
+alias ls="ls ${colorflag}"
+
+# List all files colorized in long format
+alias l="ls -lF ${colorflag}"
+
+# List all files colorized in long format, including dot files
+alias la="ls -laF ${colorflag}"
+
+# List only directories
+alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
+
 
 # Config editing binds
 alias cfv="vim ~/.vimrc"
@@ -23,3 +39,18 @@ alias cfr="vim ~/.config/ranger/rc.conf"
 alias cfm="vim ~/.muttrc"
 alias cfp="vim ~/.config/polybar/config"
 alias cfd="vim ~/.config/dunst/dunstrc"
+
+# Use i3lock when afk
+alias afk="sh ~/Scripts/lock.sh"
+
+# Reload shell
+alias reload="exec $SHELL -l"
+
+# Get week number
+alias week='date +%V'
+
+# Sped up cd navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
