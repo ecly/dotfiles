84b9d0da839c28fa6fa575432ab70f9125aa20e6
ecly <ecly@mailbox.org>
2017-09-12 08:59:45 +0200

    updated wal script for new file format

--- Scripts/wal.sh
+++ Scripts/wal.sh
@@ -2,7 +2,9 @@
 
 # Generate correct path for last used colorscheme
 scheme=$(cat ~/.cache/wal/wal)
-scheme=${scheme/./_} #replace . with _
+scheme=${scheme//\//_} #replace all / with _
+scheme=${scheme/./_} #replace single . with _
 ext=".json"
 
-wal -f "${HOME}/.cache/wal/schemes/$scheme$ext" 
+echo "$scheme"
+wal -n -f "${HOME}/.cache/wal/schemes/$scheme$ext" 
