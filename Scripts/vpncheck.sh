3c8b01f37eb44849136db5392cab6ca5b6f4f692
ecly <ecly@mailbox.org>
2017-09-23 10:10:39 +0200

    made vpn statuschecker a lot neater

---
+++ Scripts/vpncheck.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# source wal colors so we can align output colors
+# with those expected to be used by polybar
+source "${HOME}/.cache/wal/colors.sh"
+foreground_alt=$color2
+
+if pgrep -x openvpn >&/dev/null; then
+    echo %{F$foreground_alt}%{F-} on;
+else 
+    echo %{F$foreground_alt}%{F-}%{F#f00} off%{F-}; 
+fi
