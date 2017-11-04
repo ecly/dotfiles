4e5847f28f216184c220325b9c7f3807aa25c428
ecly <ecly@mailbox.org>
2017-11-04 10:13:42 +0100

    finished coding tmux script

--- Scripts/code_mux.sh
+++ Scripts/code_mux.sh
@@ -1,9 +1,14 @@
 #!/bin/sh 
 
-tmux new-session -d -s 'code'
+# start session or attach if exists
+tmux new-session -A -d -s 'code'
+
+# setup the splits
 tmux split-window -h
 tmux split-window -v 
+
+# set the active pane to the editor one
+tmux select-pane -t 0
+
+# attach to the created session
 tmux -2 attach-session -t 'code'
-tmux select-pane -t 0
-tmux swap-pane
-tmux select-pane -t 0
