f53e223c833fbc61f44ae8de1d1246de9eb143b1
ecly <ecly@mailbox.org>
2018-03-07 19:24:39 +0100

    start ncmpcpp on playlist browser

--- Scripts/music_mux.sh
+++ Scripts/music_mux.sh
@@ -1,6 +1,6 @@
-#!/bin/sh 
+#!/bin/sh
 mpc random on
 mpc repeat on
-tmux new-session -d 'ncmpcpp'
+tmux new-session -d 'ncmpcpp -s browser'
 tmux split-window -p 20 -v 'cava'\; last-pane
-tmux -2 attach-session -d   
+tmux -2 attach-session -d
