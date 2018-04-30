133f6d8dd084ba8241936a10f4bfbc117ab04630
ecly <ecly@mailbox.org>
2018-04-30 22:43:23 +0200

    tweak tmux coloring

--- Scripts/wal.sh
+++ Scripts/wal.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# Generate correct path for last used colorscheme
-scheme=$(cat ~/.cache/wal/wal)
-scheme=${scheme//\//_} #replace all / with _
-scheme=${scheme/./_} #replace single . with _
-ext=".json"
-
-echo "$scheme"
-# wal -n -f "${HOME}/.cache/wal/schemes/$scheme$ext"
 wal -n -R
