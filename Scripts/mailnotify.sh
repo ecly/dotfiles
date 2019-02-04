c54a6052c298f1a9a93559f7e52f9168f5d3b1ed
ecly <ecly@mailbox.org>
2019-02-04 16:55:24 +0100

    update mutt folder/box bindings and notifications

--- Scripts/mailnotify.sh
+++ Scripts/mailnotify.sh
@@ -6,6 +6,11 @@
 
 sound=~/Scripts/ding.ogx
 volume=40000 # value between 0 and 65536
+current_mailbox=$1
 
-notify-send 'New Email' "$1 new messages, $2 unread."\
-&& paplay --volume $volume $sound &
+# don't notify if no new/unread messages
+if [$2 != "0"] && [$3 != "0"]
+then
+    notify-send "New Email" "$2 new messages, $3 unread."\
+    && paplay --volume $volume $sound &
+fi
