6c9087395a08ee56b118fcaf4f007ff093d242be
ecly <ecly@mailbox.org>
2019-05-28 09:21:21 +0200

    increase sleep timer in optirun external monitor script for consistency

--- Scripts/work.sh
+++ Scripts/work.sh
@@ -1,6 +1,6 @@
 . /home/ecly/Scripts/keymap.sh; \
 xset b off; \
 optirun intel-virtual-output; \
-sleep 2; \
+sleep 4; \
 mons -m; \
 nohup sh ~/.config/polybar/launch.sh > /dev/null 2>&1 & # rerun polybar
