513010413462fdc94ca477e9116123e8600b708d
ecly <ecly@mailbox.org>
2019-08-05 23:41:39 +0000

    add elixir and mullvad to install

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
-#git clone https://aur.archlinux.org/yay.git
-#cd yay
-#makepkg -si
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
-yay -Syyu zsh zsh-completions zsh-syntax-highlighting tmux i3-gaps polybar rofi neovim dunst ranger mpv mpd mopidy mopidy-spotify ncmpcpp cava neomutt neofetch i3lock maim nitrogen sxiv firefox weechat python-pywal pulseaudio pulseaudio-alsa gotop networkmanager pamixer ncpamixer w3m urlscan xclip imagemagick gnupg terminus-font terminus-font-ttf highlight ffmpegthumbnailer redshift fzf ripgrep tmuxinator xbanish ttf-inconsolata noto-fonts ttf-font-awesome-4 xorg xorg-xinit zsh-syntax-highlighting zsh-completions openssh rxvt-unicode python-neovim bitwarden-bin fd kbdlight
+yay -Syyu zsh zsh-completions zsh-syntax-highlighting tmux i3-gaps polybar rofi neovim dunst ranger mpv mpd mopidy mopidy-spotify ncmpcpp cava neomutt neofetch i3lock maim nitrogen sxiv firefox weechat python-pywal pulseaudio pulseaudio-alsa gotop networkmanager pamixer ncpamixer w3m urlscan xclip imagemagick gnupg terminus-font terminus-font-ttf highlight ffmpegthumbnailer redshift fzf ripgrep tmuxinator xbanish ttf-inconsolata noto-fonts ttf-font-awesome-4 xorg xorg-xinit zsh-syntax-highlighting zsh-completions openssh rxvt-unicode python-neovim bitwarden-bin fd kbdlight elixir mullvad-vpn
 
 wal --theme base16-gruvbox-hard
