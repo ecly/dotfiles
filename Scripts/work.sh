a93e31ffebefed6dbb1b4a122f08722efa323196
ecly <ecly@mailbox.org>
2019-09-03 13:01:10 +0200

    proper polybar launch in work monitor script

--- Scripts/work.sh
+++ Scripts/work.sh
@@ -1,7 +1,7 @@
 . /home/ecly/Scripts/keymap.sh; \
 xset b off; \
 optirun intel-virtual-output; \
-sleep 4; \
+sleep 3; \
 xrandr --output eDP1 --primary --mode 1920x1080; \
 xrandr --output VIRTUAL1 --mode VIRTUAL1.446-2560x1440 --right-of eDP1; \
 xrandr --output VIRTUAL2 --mode VIRTUAL1.446-2560x1440 --right-of VIRTUAL1; \
@@ -9,7 +9,7 @@ sleep 2; \
 i3-msg "workspace 1, move workspace to output VIRTUAL1"; \
 i3-msg "workspace 2, move workspace to output VIRTUAL2"; \
 feh ~/Pictures/wallpapers/wallpaper.jpg; \
-sh ~/.config/polybar/launch.sh > /dev/null 2>&1 &
+nohup sh ~/.config/polybar/launch.sh > /dev/null 2>&1 &
 
 # remove any modes if they are still there
 # xrandr --delmode VIRTUAL1 2560x1440_60
