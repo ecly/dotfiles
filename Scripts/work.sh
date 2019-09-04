bd27305b9d6d7bc0e67dfa024f21dfac2c97d41a
ecly <ecly@mailbox.org>
2019-09-04 10:54:06 +0200

    correctly set wallpaper in work script

--- Scripts/work.sh
+++ Scripts/work.sh
@@ -8,7 +8,7 @@ xrandr --output VIRTUAL2 --mode VIRTUAL1.446-2560x1440 --right-of VIRTUAL1; \
 sleep 2; \
 i3-msg "workspace 1, move workspace to output VIRTUAL1"; \
 i3-msg "workspace 2, move workspace to output VIRTUAL2"; \
-feh ~/Pictures/wallpapers/wallpaper.jpg; \
+feh --bg-scale ~/Pictures/wallpapers/wallpaper.jpg; \
 nohup sh ~/.config/polybar/launch.sh > /dev/null 2>&1 &
 
 # remove any modes if they are still there
