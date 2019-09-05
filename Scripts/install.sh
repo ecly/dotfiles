46c8c2a3f38b9a256022b0413a155c8c7d059a5e
ecly <ecly@mailbox.org>
2019-09-05 12:52:33 +0200

    add zathura to install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,7 +2,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -Syyu zsh zsh-completions zsh-syntax-highlighting tmux i3-gaps polybar rofi neovim dunst ranger mpv mpd mopidy mopidy-spotify ncmpcpp cava neomutt neofetch i3lock maim nitrogen sxiv firefox weechat python-pywal pulseaudio pulseaudio-alsa gotop networkmanager pamixer w3m urlscan xclip imagemagick gnupg terminus-font terminus-font-ttf highlight ffmpegthumbnailer redshift fzf ripgrep tmuxinator xbanish ttf-inconsolata noto-fonts ttf-font-awesome-4 xorg xorg-xinit zsh-syntax-highlighting zsh-completions openssh rxvt-unicode python-neovim bitwarden-bin fd kbdlight elixir mullvad-vpn xf86-input-keyboard python-pylint ntp python-jedi urxvt-resize-font fd feh speedtest-cli pacmixer
+yay -Syyu zsh zsh-completions zsh-syntax-highlighting tmux i3-gaps polybar rofi neovim dunst ranger mpv mpd mopidy mopidy-spotify ncmpcpp cava neomutt neofetch i3lock maim nitrogen sxiv firefox weechat python-pywal pulseaudio pulseaudio-alsa gotop networkmanager pamixer w3m urlscan xclip imagemagick gnupg terminus-font terminus-font-ttf highlight ffmpegthumbnailer redshift fzf ripgrep tmuxinator xbanish ttf-inconsolata noto-fonts ttf-font-awesome-4 xorg xorg-xinit zsh-syntax-highlighting zsh-completions openssh rxvt-unicode python-neovim bitwarden-bin fd kbdlight elixir mullvad-vpn xf86-input-keyboard python-pylint ntp python-jedi urxvt-resize-font fd feh speedtest-cli pacmixer python-pip python-regex python-tqdm zathura zathura-pdf-mupdf
 
 # apply theme
 wal --theme base16-gruvbox-hard
