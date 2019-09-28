1e5fe508fe9fdd3f6d1050ee2d8b7073e1cb77eb
ecly <ecly@mailbox.org>
2019-09-28 23:57:51 +0200

    ensure no beeping when using keymap script

--- Scripts/keymap.sh
+++ Scripts/keymap.sh
@@ -6,3 +6,6 @@
 
 # Always bind caps to escape and keybind to shuffle key layouts
 setxkbmap -option caps:escape; setxkbmap -option grp:switch,grp:win_space_toggle,grp_led:scroll us,dk
+
+# Make sure beeping is disabled
+xset b off
