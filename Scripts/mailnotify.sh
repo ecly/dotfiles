3393464194129b842c7ec82b20bc19383823e6a3
ecly <ecly@mailbox.org>
2019-12-13 09:18:01 +0100

    update mailnotify script

--- Scripts/mailnotify.sh
+++ Scripts/mailnotify.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Expected input
-# $1 Number of new
-# $2 Number of unread
+# $1 Mailbox name
+# $2 Number of new
+# $3 Number of unread
 
 sound=~/Scripts/ding.ogx
 volume=40000 # value between 0 and 65536
 current_mailbox=$1
+new_messages=$2
+unread_messages=$3
 
-# don't notify if no new/unread messages
-if [$2 != "0"] && [$3 != "0"]
+# don't notify if no new messages
+if [ $new_messages -ne 0 ]
 then
-    notify-send "New Email" "$2 new messages, $3 unread."\
-    && paplay --volume $volume $sound &
+    notify-send "New Email in $current_mailbox" "$2 new messages, $3 unread."\ && paplay --volume $volume $sound &
 fi
