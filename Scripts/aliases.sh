e491526f0c7d0cbd4c210f7f000fea21b547f086
ecly <ecly@mailbox.org>
2020-02-09 11:55:12 +0100

    fix shellcheck issues for aliases.sh

--- Scripts/aliases.sh
+++ Scripts/aliases.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 # ALIASES
 alias dot='git --git-dir=$HOME/.dots.git/ --work-tree=$HOME' # dotfile handling
 alias shutdown="systemctl poweroff"
+# shellcheck disable=SC2139
 alias {vi,vim}="nvim"
 alias todo="nvim ~/Dropbox/IRL/todo.txt"
 alias poly="nohup sh ~/.config/polybar/launch.sh > /dev/null 2>&1 &"
@@ -22,24 +24,28 @@ alias ftree="find | sed 's|[^/]*/|- |g'"
 
 # Detect which `ls` flavor is in use
 if ls --color > /dev/null 2>&1; then # GNU `ls`
-  colorflag="--color"
+  colorflag='--color'
 else # OS X `ls`
-  colorflag="-G"
+  colorflag='-G'
 fi
 
 # ask when deleting multiple
 alias rm="rm -I"
 
 # Basic colored ls
+# shellcheck disable=SC2139
 alias ls="ls ${colorflag} --human-readable --group-directories-first --classify"
 
 # List all files colorized in long format
+# shellcheck disable=SC2139
 alias l="ls -lF ${colorflag}"
 
 # List all files colorized in long format, including dot files
+# shellcheck disable=SC2139
 alias la="ls -laF ${colorflag}"
 
 # List only directories
+# shellcheck disable=SC2139
 alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
 
 # Some shorthand launching
@@ -48,6 +54,7 @@ alias ta="tmux attach-session"
 alias u="urxvt"
 
 # All pagers except less bound to vless
+# shellcheck disable=SC2139
 alias {vl,more,most,view}="vless"
 
 # Config editing binds
@@ -68,7 +75,7 @@ alias csgo="sh ~/Scripts/vibrance.sh"
 alias afk="sh ~/Scripts/lock.sh"
 
 # Reload shell
-alias reload="exec $SHELL -l"
+alias reload='exec $SHELL -l'
 
 # Used for grepping cars from the zipped
 # Danish motorregister
@@ -90,6 +97,7 @@ alias scratch="urxvt -name scratch -e tmux & disown"
 
 # Remove all local branches that 'gone' on the remote. Lanuguage dependent
 # https://stackoverflow.com/questions/7726949/remove-local-branches-no-longer-on-remote/38404202#38404202
+# shellcheck disable=SC2139
 alias gprune="git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d"
 
 # Shortcut for running work script
@@ -99,7 +107,7 @@ alias vm="sh ~/Scripts/vm.sh"
 alias home="sh ~/Scripts/home.sh"
 
 # Start an instance of firefox with no profile
-alias fx="firefox --new-instance --profile $(mktemp -d)"
+alias fx='firefox --new-instance --profile $(mktemp -d)'
 
 # Check what users are online on twitch for given user"
 alias online="python3 ~/Scripts/check_online.py"
