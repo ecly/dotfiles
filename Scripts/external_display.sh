cdd7e1127ab4b72a6383bea780ef429425b9af24
ecly <ecly@mailbox.org>
2020-06-13 21:54:35 +0200

    remove unused monitor setup scripts

--- Scripts/external_display.sh
+++
@@ -1,1 +0,0 @@
-xrandr --output VIRTUAL3 --mode VIRTUAL3.453-2560x1440 --output eDP1 --same-as VIRTUAL3 --scale 1.33x1.33
--- Scripts/music_mux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mpc random on
-mpc repeat on
-tmux -2 new-session -A -s ncmpcpp 'ncmpcpp -s browser'
-tmux split-window -p 20 -v 'cava'\; last-pane
