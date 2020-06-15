fd4089f8fed46c3a53f7ef1eda2491ae07300fb3
ecly <ecly@mailbox.org>
2020-06-15 09:38:12 +0200

    remove feh from work scripts

--- Scripts/work.sh
+++ Scripts/work.sh
@@ -16,7 +16,6 @@ i3-msg "workspace 1, move workspace to output VIRTUAL1" > /dev/null; \
 i3-msg "workspace 2, move workspace to output VIRTUAL2" > /dev/null; \
 i3-msg "workspace 3, move workspace to output eDP1" > /dev/null; \
 i3-msg "workspace 4, move workspace to output VIRTUAL1" > /dev/null; \
-feh --bg-scale ~/Pictures/wallpapers/wallpaper.jpg; \
 nohup sh ~/.config/polybar/launch.sh > /dev/null 2>&1 &
 
 # remove any modes if they are still there
