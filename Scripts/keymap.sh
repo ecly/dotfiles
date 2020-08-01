e8934d1013abad798c6fc3902dd0b7595dd47235
ecly <ecly@mailbox.org>
2020-08-01 16:29:08 +0200

    cleanup more unused settings/scripts

--- Scripts/keymap.sh
+++ Scripts/keymap.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# if [ "$HOSTNAME" = trsh ]; then
-    # On laptop swap alt and win
-    # setxkbmap -option altwin:swap_lalt_lwin
-# fi
-
 # Always bind caps to escape and keybind to shuffle key layouts
 setxkbmap -option caps:escape; setxkbmap -option grp:switch,grp:win_space_toggle,grp_led:scroll us,dk
 
