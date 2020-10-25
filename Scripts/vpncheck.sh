8d74592448085ea60509674b7fcb12f0ba162a8e
ecly <ecly@mailbox.org>
2020-10-25 18:27:25 +0100

    add robustness to mullvad not installed for vpncheck

--- Scripts/vpncheck.sh
+++ Scripts/vpncheck.sh
@@ -3,10 +3,17 @@
 # source wal colors so we can align output colors
 # with those expected to be used by polybar
 source "${HOME}/.cache/wal/colors.sh"
-foreground_alt=$color2
+foreground_alt="$color2"
+
+on_msg="%{F$foreground_alt}%{F-} on"
+off_msg="%{F$foreground_alt}%{F-}%{F#f00} off%{F-}"
+
+if ! [ -x "$(command -v mullvad)" ]; then
+    echo "$off_msg"
+fi
 
 if mullvad status | grep "Connected" >&/dev/null; then
-    echo %{F$foreground_alt}%{F-} on;
+    echo "$on_msg"
 else
-    echo %{F$foreground_alt}%{F-}%{F#f00} off%{F-};
+    echo "$off_msg"
 fi
