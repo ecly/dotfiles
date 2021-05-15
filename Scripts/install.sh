20e794c8471ca6bcf7bdaca14f52fedaed5b06e9
ecly <ecly@mailbox.org>
2021-05-15 21:57:46 +0200

    add extra python language server deps to install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -81,7 +81,8 @@ yay -Syyu --noconfirm \
 
 # Handle Python specific installs with pipx
 pip3 install pipx --user
-pipx install python-language-server
+pipx install "python-language-server[pydocstyle,pylint]" \
+    && pipx inject python-language-server pyls-mypy pyls-isort pyls-black
 pipx install poetry
 pipx install black
 pipx install isort
