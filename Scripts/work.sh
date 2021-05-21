ade583c7e207707ad6398847ff7625fa0d3f8a74
ecly <ecly@mailbox.org>
2021-05-21 17:01:19 +0200

    improve work script for multi screen and add logging

--- Scripts/work.sh
+++ Scripts/work.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-# Test whether intel-virtual-output is running and start it.
+
+# If IVO is already running, we kill the existing instance
+# reusing the existing one seems to cause instability
 ivo_process=`pgrep intel-virtual-asdf`
+set -e
 if [ ! -z "$ivo_process" ]; then
-    kill -9 $ivo_process
+    echo "Killing existing IVO process"
+    kill -9 "$ivo_process"
     sleep 1
-    make sure that vsync is disabled
-    vblank_mode=0 optirun -b primus intel-virtual-output
-    sleep 3
+    echo "Turning off GPU with bbswitch"
+    sudo tee /proc/acpi/bbswitch <<< OFF
 fi
 
+# make sure that vsync is disabled
+echo "Starting IVO..."
+vblank_mode=0 primusrun intel-virtual-output
+sleep 3
+echo "Stareted IVO.
 
+echo "Setting up displays with XRANDR..."
 # patched xrandr for pixel perfect 2x scaling for 4k
 PATCHED="$HOME/Scripts/xrandr/xrandr"
 if [[ -f $PATCHED ]] && [[ 1 -eq 2 ]]; then
@@ -22,8 +31,10 @@ else
     xrandr --output VIRTUAL2 --mode 2560x1440 --right-of VIRTUAL1
 fi
 
+echo "Setting up keymap..."
 sh /home/ecly/Scripts/keymap.sh
 
+echo "Setting up i3 workspaces..."
 sleep 2
 i3-msg "workspace 1 output VIRTUAL1" > /dev/null; \
 i3-msg "workspace 2 output VIRTUAL2" > /dev/null; \
@@ -32,5 +43,9 @@ i3-msg "workspace 4 output VIRTUAL1" > /dev/null; \
 i3-msg "workspace 1, move workspace to output VIRTUAL1" > /dev/null; \
 i3-msg "workspace 2, move workspace to output VIRTUAL2" > /dev/null; \
 i3-msg "workspace 3, move workspace to output eDP1" > /dev/null; \
-i3-msg "workspace 4, move workspace to output VIRTUAL1" > /dev/null; \
+i3-msg "workspace 4, move workspace to output VIRTUAL1" > /dev/null
+
+echo "Setting up polybar..."""
 nohup sh ~/.config/polybar/launch.sh > /dev/null 2>&1 &
+
+echo "Finished!"
