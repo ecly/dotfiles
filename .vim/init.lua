853ca7c0bf1c382fda96d4eaa4e2eae28fd177d9
Emil Lynegaard <eml@languagewire.com>
2022-08-13 22:04:54 +0200

    update indent_blankline configuration

--- .vim/init.lua
+++ .vim/init.lua
@@ -148,7 +148,16 @@ require('packer').startup(function()
     use 'nvim-lualine/lualine.nvim'
     use 'akinsho/nvim-bufferline.lua'
 
-    use 'lukas-reineke/indent-blankline.nvim'
+    vim.opt.list = true
+    vim.opt.listchars:append "space:⋅"
+    vim.opt.listchars:append "eol:↴"
+    require("indent_blankline").setup {
+        -- for example, context is off by default, use this to turn it on
+        show_current_context = true,
+        show_current_context_start = true,
+        show_end_of_line = true,
+        space_char_blankline = " ",
+    }
 
     -- Tpope plugins
     use 'tpope/vim-fugitive' -- Git commands in nvim
