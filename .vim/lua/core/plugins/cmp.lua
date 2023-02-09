4b9a10a8739c3a8ea72db7d0781cccdae4acb3d3
Emil Lynegaard <eml@languagewire.com>
2023-02-09 13:30:27 +0100

    fix cmp tab issues for empty lines

--- .vim/lua/core/plugins/cmp.lua
+++ .vim/lua/core/plugins/cmp.lua
@@ -1,4 +1,17 @@
 -- better completion support
+
+-- functions for super tab like support
+-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#vim-vsnip
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 local M = {
     'hrsh7th/nvim-cmp',
     dependencies = {
