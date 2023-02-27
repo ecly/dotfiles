63ccfa5de8ed24dcf150d00be1ee863311dbda39
Emil Lynegaard <eml@languagewire.com>
2023-02-27 15:36:25 +0100

    increase whick-key timeout

--- .vim/lua/core/plugins/which-key.lua
+++ .vim/lua/core/plugins/which-key.lua
@@ -3,7 +3,7 @@ local M = {
     'folke/which-key.nvim',
     config = function()
         vim.o.timeout = true
-        vim.o.timeoutlen = 100
+        vim.o.timeoutlen = 200
 
         require("which-key").setup({})
     end
