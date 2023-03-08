d31d9580533e83a918e7b9d033d8efa5fa38135a
Emil Lynegaard <eml@languagewire.com>
2023-03-08 13:12:58 +0100

    disable noice notify integration

--- .vim/lua/core/plugins/noice.lua
+++ .vim/lua/core/plugins/noice.lua
@@ -5,7 +5,7 @@ local M = {
         "MunifTanjim/nui.nvim", -- OPTIONAL:
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify"
+        -- "rcarriga/nvim-notify"
     },
     config = function()
         require("noice").setup({
