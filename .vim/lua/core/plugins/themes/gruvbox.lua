f6c8bb8b3c4045507efd6c0267ca2270df9fd432
Emil Lynegaard <eml@languagewire.com>
2023-04-14 21:28:53 +0200

    update plugins

--- .vim/lua/core/plugins/themes/gruvbox.lua
+++ .vim/lua/core/plugins/themes/gruvbox.lua
@@ -2,7 +2,7 @@ require("gruvbox").setup({
     undercurl = true,
     underline = true,
     bold = true,
-    italic = false,
+    italic = {strings = false, operators = false, comments = true},
     strikethrough = true,
     invert_selection = false,
     invert_signs = false,
