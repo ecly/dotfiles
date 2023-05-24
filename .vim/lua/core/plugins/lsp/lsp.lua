ad66002b9cf4289133e2804efa26595c795115cd
Emil Lynegaard <eml@languagewire.com>
2023-05-24 23:00:11 +0200

    downgrade lua_ls version following https://github.com/LuaLS/lua-language-server/issues/2123

--- .vim/lua/core/plugins/lsp/lsp.lua
+++ .vim/lua/core/plugins/lsp/lsp.lua
@@ -7,43 +7,43 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local servers = {
-  "bashls",
-  "dockerls",
-  "jsonls",
-  "marksman",
-  -- "pyright", -- not currently working
-  "pylsp",
-  "lua_ls",
-  "terraformls",
-  "texlab",
-  "tsserver",
-  "yamlls",
+    "bashls",
+    "dockerls",
+    "jsonls",
+    "marksman",
+    -- "pyright", -- not currently working
+    "pylsp",
+    "lua_ls",
+    "terraformls",
+    "texlab",
+    "tsserver",
+    "yamlls",
 }
 
 -- this needs to be called before setting up lsps
 require("mason").setup({})
 require("mason-lspconfig").setup({
-  automatic_installation = true,
+    automatic_installation = true,
 })
 
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup({
-      on_attach = function(client, bufnr)
-        lsp_utils.custom_lsp_attach(client, bufnr)
-      end,
-      -- before_init = function(_, config)
-      --   if lsp == "pyright" then
-      --     config.settings.python.pythonPath = utils.get_python_path(config.root_dir)
-      --   end
-      -- end,
-      capabilities = capabilities,
-      flags = { debounce_text_changes = 150 },
+        on_attach = function(client, bufnr)
+            lsp_utils.custom_lsp_attach(client, bufnr)
+        end,
+        -- before_init = function(_, config)
+        --   if lsp == "pyright" then
+        --     config.settings.python.pythonPath = utils.get_python_path(config.root_dir)
+        --   end
+        -- end,
+        capabilities = capabilities,
+        flags = { debounce_text_changes = 150 },
     })
 end
 
 local venv = utils.exists("./.venv/") and "./.venv" or nil
 nvim_lsp.pylsp.setup({
-    cmd = {"pylsp", "--log-file", "/home/ecly/pylsp.log", "-v"},
+    cmd = { "pylsp", "--log-file", "/home/ecly/pylsp.log", "-v" },
     capabilities = capabilities,
     on_attach = function(client, _)
         client.server_capabilities.documentFormattingProvider = false
@@ -51,7 +51,7 @@ nvim_lsp.pylsp.setup({
     end,
     cmd_env = {
         VIRTUAL_ENV = venv,
-        PATH = require'lspconfig/util'.path.join(venv, 'bin') .. ':' ..
+        PATH = require 'lspconfig/util'.path.join(venv, 'bin') .. ':' ..
             vim.env.PATH
     },
     enable = true,
@@ -60,20 +60,20 @@ nvim_lsp.pylsp.setup({
             configurationSources = {},
             plugins = {
                 jedi = {
-                    extra_paths = {"./dags"},
+                    extra_paths = { "./dags" },
                     environment = venv,
                     enabled = true
                 },
-                pydocstyle = {enabled = false},
-                pylint = {enabled = false},
-                rope = {enabled = false},
-                pyflakes = {enabled = false},
-                pycodestyle = {enabled = false},
-                yapf = {enabled = false},
-                mccabe = {enabled = false},
-                pylsp_mypy = {enabled = false},
-                pylsp_black = {enabled = false},
-                pylsp_isort = {enabled = false}
+                pydocstyle = { enabled = false },
+                pylint = { enabled = false },
+                rope = { enabled = false },
+                pyflakes = { enabled = false },
+                pycodestyle = { enabled = false },
+                yapf = { enabled = false },
+                mccabe = { enabled = false },
+                pylsp_mypy = { enabled = false },
+                pylsp_black = { enabled = false },
+                pylsp_isort = { enabled = false }
             }
         }
     }
@@ -83,6 +83,6 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
     vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
         -- Enable underline, use default values
         underline = true,
-        virtual_text = {spacing = 0, prefix = '■'},
+        virtual_text = { spacing = 0, prefix = '■' },
         update_in_insert = false
     })
