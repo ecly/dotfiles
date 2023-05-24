295984a83d0b250e7db3137b0e98431fab6c4dad
Emil Lynegaard <eml@languagewire.com>
2023-05-24 14:02:50 +0200

    update cokeline github reference

--- .vim/lua/core/plugins/cokeline.lua
+++ .vim/lua/core/plugins/cokeline.lua
@@ -1,6 +1,6 @@
 -- bufferline matching the statusline
 local M = {
-    'noib3/nvim-cokeline',
+    'willothy/nvim-cokeline',
     dependencies = {'nvim-tree/nvim-web-devicons'},
     config = function()
       require('cokeline').setup()
