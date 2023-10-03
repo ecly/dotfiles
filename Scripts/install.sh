ad3f6f327591562bf237feafab8bcc58df5b8ad5
Emil Lynegaard <ecly@mailbox.org>
2023-10-03 17:30:56 +0200

    update install.sh for macos

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,85 +1,134 @@
 !/bin/bash
 set -e
 
-# install yay if not already installed
-if ! command -v yay > /dev/null; then
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-    cd ..
-    rm -rf yay
-fi
 
-yay -Syyu --noconfirm \
-    zsh \
-    antibody \
-    python-pip \
-    tmux \
-    i3-gaps \
-    polybar \
-    rofi \
-    neovim \
-    python-pynvim \
-    neovim-remote \
-    dunst \
-    ranger \
-    mpv \
-    i3lock \
-    xss-lock \
-    maim \
-    sxiv \
-    firefox \
-    pulseaudio \
-    pulseaudio-alsa \
-    pulsemixer \
-    gotop \
-    networkmanager \
-    neomutt \
-    w3m \
-    urlscan \
-    xclip \
-    imagemagick \
-    gnupg \
-    noto-fonts \
-    terminus-font-ttf \
-    nerd-fonts-terminus \
-    highlight \
-    ffmpegthumbnailer \
-    redshift \
-    fzf \
-    ripgrep \
-    tmuxinator \
-    xbanish \
-    xorg \
-    xorg-xinit \
-    openssh \
-    bitwarden-bin \
-    fd \
-    kbdlight \
-    elixir \
-    mullvad-vpn-bin \
-    ntp \
-    alacritty \
-    fd \
-    zathura \
-    zathura-pdf-mupdf \
-    wget \
-    zip \
-    unzip \
-    npm \
-    nm-connection-editor \
-    ctags \
-    git-delta \
-    xdotool \
-    mons \
-    sc-im \
-    ctop-bin \
-    pyenv \
-    elixir-ls \
-    lua-language-server \
-    lua-format \
-    terraform-ls \
-    ncdu
+if [[ "$OSTYPE" == "darwin"* ]]; then
+
+    if ! command -v brew > /dev/null; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        source ~/.zprofile
+    fi
+
+    python
+
+    brew install koekeishiya/formulae/skhd
+
+    brew install \
+        antidote \
+        betterdisplay \
+        bitwarden \
+        ca-certificates \
+        cmake \
+        universal-ctags \
+        discord \
+        docker \
+        docker-completion \
+        docker-compose \
+        fd \
+        firefox \
+        fzf \
+        git-delta \
+        gnu-sed \
+        ncdu \
+        neovim \
+        neovim-remote \
+        nvm \
+        protobuf \
+        pyenv \
+        ripgrep \
+        signal \
+        spotify \
+        tmux \
+        tmuxinator \
+        tree-sitter \
+        tresort \
+        unnaturalscrollwheels \
+        wezterm \
+        wget \
+
+
+else
+    # install yay if not already installed
+    if ! command -v yay > /dev/null; then
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si
+        cd ..
+        rm -rf yay
+    fi
+
+
+    yay -Syyu --noconfirm \
+        alacritty \
+        antibody \
+        bitwarden-bin \
+        ctags \
+        ctop-bin \
+        dunst \
+        elixir \
+        elixir-ls \
+        fd \
+        fd \
+        ffmpegthumbnailer \
+        firefox \
+        fzf \
+        git-delta \
+        gnupg \
+        gotop \
+        highlight \
+        i3-gaps \
+        i3lock \
+        imagemagick \
+        kbdlight \
+        lua-format \
+        lua-language-server \
+        maim \
+        mons \
+        mpv \
+        mullvad-vpn-bin \
+        ncdu
+        neomutt \
+        neovim \
+        neovim-remote \
+        nerd-fonts-terminus \
+        networkmanager \
+        nm-connection-editor \
+        noto-fonts \
+        npm \
+        ntp \
+        openssh \
+        polybar \
+        pulseaudio \
+        pulseaudio-alsa \
+        pulsemixer \
+        pyenv \
+        python-pip \
+        python-pynvim \
+        ranger \
+        redshift \
+        ripgrep \
+        rofi \
+        sc-im \
+        sxiv \
+        terminus-font-ttf \
+        terraform-ls \
+        tmux \
+        tmuxinator \
+        unzip \
+        urlscan \
+        w3m \
+        wget \
+        xbanish \
+        xclip \
+        xdotool \
+        xorg \
+        xorg-xinit \
+        xss-lock \
+        zathura \
+        zathura-pdf-mupdf \
+        zip \
+        zsh \
+fi
 
 
 # Handle Python specific installs with pipx
@@ -87,11 +136,10 @@ pyenv install 3.11.5
 pyenv global 3.11.5
 pyenv shell 3.11.5
 pip3 install pipx --user
-pipx install "python-lsp-server[all]" \
-    && pipx inject python-lsp-server pylsp-mypy pyls-isort python-lsp-black black"
 pipx install poetry
 pipx install pywal
 pipx install vint
+pipx install pywal
 
 # apply theme
 wal --theme base16-gruvbox-medium
@@ -116,3 +164,6 @@ if [ -e "$1" ]; then
         biber \
         texlive-lang
 fi
+
+mkdir -p ~/.cache/zsh
+touch ~/.cache/zsh/history
