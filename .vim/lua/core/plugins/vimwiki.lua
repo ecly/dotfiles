8b22ae048579e35bb0b21b0e46cbb4a722a28133
Emil Lynegaard <ecly@mailbox.org>
2024-05-23 15:50:44 +0200

    fix vimwiki binds overriding

--- .vim/lua/core/plugins/vimwiki.lua
+++ .vim/lua/core/plugins/vimwiki.lua
@@ -1,20 +1,23 @@
 -- support for wiki handling through nvim
 local M = {
-        'vimwiki/vimwiki',
-        config = function()
-            vim.g.vimwiki_list = {
-                {
-                    path = '~/Documents/work/vimwiki/',
-                    syntax = 'markdown',
-                    ext = '.md'
-                },
-                {
-                    path = '~/Documents/irl/vimwiki/',
-                    syntax = 'markdown',
-                    ext = '.md'
-                }
-            }
-        end
+	"vimwiki/vimwiki",
+	config = function()
+		vim.g.vimwiki_list = {
+			{
+				path = "~/Documents/work/vimwiki/",
+				syntax = "markdown",
+				ext = ".md",
+			},
+			{
+				path = "~/Documents/irl/vimwiki/",
+				syntax = "markdown",
+				ext = ".md",
+			},
+		}
+		vim.g.vimwiki_key_mappings = {
+			table_mappings = 0,
+		}
+	end,
 }
 
 return M
