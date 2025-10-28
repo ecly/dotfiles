27b272489550818d9b0f19b42783bb2e372abd69
Emil Lynegaard <ecly@mailbox.org>
2025-10-28 09:09:39 +0100

    tweak lualine config and make parts explicit

--- .vim/lua/core/plugins/lualine.lua
+++ .vim/lua/core/plugins/lualine.lua
@@ -5,6 +5,19 @@ local M = {
         local settings = require("core.settings")
         require('lualine').setup {
             options = {theme = settings.theme},
+            -- mostly default with lower refresh.
+            -- https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#default-configuration
+            refresh = {
+                statusline = 1000,
+                tabline = 1000,
+                winbar = 1000,
+                refresh_time = 32, -- ~30
+                events = {
+                    'WinEnter', 'BufEnter', 'BufWritePost', 'SessionLoadPost',
+                    'FileChangedShellPost', 'VimResized', 'Filetype',
+                    'CursorMoved', 'CursorMovedI', 'ModeChanged'
+                }
+            },
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch'},
